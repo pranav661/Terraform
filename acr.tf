5dfa2e1107fc1e120e1f06adddb015d4b76471cc
pranav661 <37332556+pranav661@users.noreply.github.com>
2019-02-08 12:39:48 +0530

    Delete acr.tf

--- acr.tf
+++
@@ -1,9 +0,0 @@
-# have removed georeplication_locations as standard sku does not support it. This requires premium sku
-# name of acr must be unique globally
-resource "azurerm_container_registry" "acr" {
-  name                     = "acrtestterraformnew1"
-  resource_group_name      = "${azurerm_resource_group.k8s.name}"
-  location                 = "${azurerm_resource_group.k8s.location}"
-  sku                      = "Standard"
-  admin_enabled            = true
-}
